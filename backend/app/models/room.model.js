35ac19724f6c2a181209918d3799ecafd1cae870
Ryan Dunning <ryandunning@Ryans-MacBook-Air-2.local>
2022-10-17 14:32:03 -0400

    backend for room selection

---
+++ backend/app/models/room.model.js
@@ -0,0 +1,27 @@
+//room model
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const roomSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    capacity: {
+        type: Number,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+});
+module.exports = mongoose.model("Room", roomSchema);
---
+++ backend/app/routes/room.routes.js
@@ -0,0 +1,15 @@
+//room routes
+const { authJwt } = require("../middleware");
+const controller = require("../controllers/room.controller");
+module.exports = function (app) {
+    app.use(function (req, res, next) {
+        res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
+    app.get("/api/rooms", [authJwt.verifyToken], controller.getRooms);
+    app.post("/api/rooms", [authJwt.verifyToken], controller.createRoom);
+    app.delete("/api/rooms/:roomId", [authJwt.verifyToken], controller.deleteRoom);
+    }
